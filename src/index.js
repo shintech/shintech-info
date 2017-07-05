366db2eb27790116f9c5525e464be469fb3eceab
Michael Prather <mprather1@gmail.com>
2017-07-05 01:18:21 +0000

    packageName

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import chalk from 'chalk'
 
-const packageDir = process.env.ROOT_DIR + 'package.json'
-const pkg = require(packageDir)
-const packageName = pkg.name
-const packageVersion = pkg.version
-
 export function getAppInfo (options) {
-  const { port, logger } = options
+  const { port, logger, packageDir } = options
+
+  const packageName = packageDir.name
+  const packageVersion = packageDir.version
+
   logger.info(`${chalk.bgBlack.cyan(packageName)} ver.${chalk.bgBlack.green(packageVersion)} listening on port ${chalk.bgBlack.yellow(port)}`)
 }
